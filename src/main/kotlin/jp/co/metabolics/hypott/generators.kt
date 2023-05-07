94fe6902ae96a3e2451c3419704ed70b4c43d8eb
YAMADA Masaki <masaki@metabolics.co.jp>
2023-05-07 11:40:06 +0900

    原始値の生成を追加した
    原始値の中でも以下はサポートしない
    - unsigned は Jackson ObjectMapper がサポートしていない
    - char は非文字を扱えない

--- src/main/kotlin/jp/co/metabolics/hypott/generators.kt
+++ src/main/kotlin/jp/co/metabolics/hypott/generators.kt
@@ -5,9 +5,30 @@ import kotlin.reflect.KType
 
 fun generateValue(type: KType, random: Random): Any? {
   return when (type.toString()) {
-    "kotlin.Int" -> generateInt(random)
+    "kotlin.Byte" -> random.nextBytes(1)[0]
+    "kotlin.Short" -> random.nextInt(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt()).toShort()
+    "kotlin.Int" -> random.nextInt()
+    "kotlin.Long" -> random.nextLong()
+    /* unsigned numbers are not yet allowed in Jackson */
+    // "kotlin.UByte" -> random.nextUBytes(1)[0]
+    // "kotlin.UShort" -> random.nextUInt(UShort.MIN_VALUE.toUInt(), UShort.MAX_VALUE.toUInt()).toUShort()
+    // "kotlin.UInt" -> random.nextUInt()
+    // "kotlin.ULong" -> random.nextULong()
+    "kotlin.Float" -> random.nextFloat()
+    "kotlin.Double" -> random.nextDouble()
+    "kotlin.Boolean" -> random.nextBoolean()
+    // this cause java.lang.IllegalArgumentException when a char is invalid as Character (文字)
+    // "kotlin.Char" -> random.nextInt(Char.MIN_VALUE.digitToInt(), Char.MAX_VALUE.digitToInt())
+    // "kotlin.String" -> // ToDo
+    // "kotlin.Array" -> // ToDo
+    // "kotlin.ByteArray" -> // ToDo
+    // "kotlin.ShortArray" -> // ToDo
+    // "kotlin.IntArray" -> // ToDo
+    // "kotlin.LongArray" -> // ToDo
+    // "kotlin.UByteArray" -> // ToDo
+    // "kotlin.UShortArray" -> // ToDo
+    // "kotlin.UIntArray" -> // ToDo
+    // "kotlin.ULongArray" -> // ToDo
     else -> null
   }
 }
-
-fun generateInt(random: Random): Int = random.nextInt()
--- src/test/kotlin/jp/co/metabolics/hypott/ForAnyKtTest.kt
+++ src/test/kotlin/jp/co/metabolics/hypott/ForAnyKtTest.kt
@@ -8,18 +8,41 @@ import kotlin.random.Random
 class ForAnyKtTest {
   private val seed = (Math.random() * 10E19).toLong()
   private lateinit var hypott: Hypott
+  private lateinit var random: Random
 
   @BeforeEach
   fun beforeEach() {
     hypott = Hypott(seed = seed)
+    random = Random(seed)
   }
 
   @Test
-  fun testForAny() {
-    data class TestInt(val i: Int)
+  // this may depend on test-target implementation
+  fun testForAnyPrimitives() {
+    data class PrimitivesFixture(
+      val b: Byte, val s: Short, val i: Int, val l: Long,
+      // val ub: UByte, val us: UShort, val ui: UInt, val ul: ULong,
+      val f: Float, val d: Double, val boolean: Boolean,
+    )
 
-    val instance = hypott.forAny(TestInt::class)
-    println("instance: $instance")
-    assertEquals(Random(seed).nextInt(), instance.i)
+    // Note: the order of evaluation (alphabetically sorted) is crucial
+    val expectation = PrimitivesFixture(
+      b = random.nextBytes(1)[0],
+      boolean = random.nextBoolean(),
+      d = random.nextDouble(),
+      f = random.nextFloat(),
+      i = random.nextInt(),
+      l = random.nextLong(),
+      s = random.nextInt(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt()).toShort(),
+      /*
+      ub = random.nextUBytes(1)[0],
+      ui = random.nextUInt(),
+      ul = random.nextULong(),
+      us = random.nextUInt(UShort.MIN_VALUE.toUInt(), UShort.MAX_VALUE.toUInt()).toUShort(),
+       */
+    )
+
+    val actual = hypott.forAny(PrimitivesFixture::class)
+    assertEquals(expectation, actual)
   }
 }
